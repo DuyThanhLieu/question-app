c341bf5118619bbada2fd69ca658b8d79a0abdd3
Thanh Lieu <duythanhlieu2001@gmail.com>
2024-01-18 17:56:55 +0700

    fix scrollbar react-perfect-scrollbar@1.5.8

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,7 +4,7 @@ import { FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
-
+import PerfectScrollbar from 'react-perfect-scrollbar';
 
 
 const Admin = (props) => {
@@ -19,11 +19,14 @@ const Admin = (props) => {
                 <div className="admin-header">
                     <FaBars onClick={() => setCollapsed(!collapsed)} />
                 </div>
+
                 <div className="admin-main">
-                    <Outlet />
+                    <PerfectScrollbar>
+                        <Outlet />
+                    </PerfectScrollbar>
                 </div>
 
-
+                {/* thanh scrollbar */}
             </div>
 
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'nprogress/nprogress.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout';
